bb233bd385fef9f565a2ecdbfe6ef047018aa9b1
Kelly Tao <meganiherb@gmail.com>
2024-04-13 16:08:06 -0700

    moving files a little bit

--- src/Tools.h
+++ src/Tools.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "imgui Support/imconfig.h"
-#include "imgui Support/imgui.h"
-#include "imgui Support/imgui_impl_dx11.h"
-#include "imgui Support/imgui_impl_win32.h"
-#include "imgui Support/imgui_internal.h"
-#include "imgui Support/imstb_rectpack.h"
-#include "imgui Support/imstb_textedit.h"
-#include "imgui Support/imstb_truetype.h"
+#include "../imgui Support/imconfig.h"
+#include "../imgui Support/imgui.h"
+#include "../imgui Support/imgui_impl_dx11.h"
+#include "../imgui Support/imgui_impl_win32.h"
+#include "../imgui Support/imgui_internal.h"
+#include "../imgui Support/imstb_rectpack.h"
+#include "../imgui Support/imstb_textedit.h"
+#include "../imgui Support/imstb_truetype.h"
 #include <d3d11.h>
 #pragma comment(lib, "d3d11.lib")
 
